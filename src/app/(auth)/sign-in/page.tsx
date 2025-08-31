3c113e9a594db0f6bc87231cf4ae4bb6975704e8
avishiprasad <prasadavishi@gmail.com>
2025-08-31 11:14:37 +0530

    signup page completed

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,22 +1,10 @@
-'use client'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import Link from "next/link"
-import { useState } from "react"
-import { useDebounceValue } from 'usehooks-ts'
-
+import React from 'react'
 
 const page = () => {
-  const [username,setUsername]=useState('')
-  const [usernameMessage, setUsernameMessage]=useState('')
-  const [isCheckingUsername,setIsCheckingUsername]=useState(false)
-  const [isSubmitting,setIsSubmitting]=useState(false)
-
-  const debouncedUsername=useDebounceValue(username,300)
-
   return (
-    <div>page</div>
+    <div>
+      sign in page
+    </div>
   )
 }
 
